02e55035ce36d94f452f979b757cb0b504d53caf
Daniel Silva <dodosilva51@gmail.com>
2020-10-14 13:13:33 -0300

    Player idle animation and script using dge

--- main/player.go
+++ main/player.go
@@ -1,8 +1,23 @@
+components {
+  id: "player"
+  component: "/main/player.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"\"\n"
-  "default_animation: \"\"\n"
+  data: "tile_set: \"/main/atlas.atlas\"\n"
+  "default_animation: \"player_idle\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
