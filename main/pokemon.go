ad1f7004cd8f84129f3f046d1f8624c56c3b472e
Daniel Silva <dodosilva51@gmail.com>
2020-10-31 20:52:57 -0300

    changed player walk animation, added lua global module and pokemon first interaction

--- main/pokemon.go
+++ main/pokemon.go
@@ -1,23 +1,8 @@
-components {
-  id: "player"
-  component: "/main/player.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
-  id: "player_sprite"
+  id: "pokemon_sprite"
   type: "sprite"
-  data: "tile_set: \"/main/player.atlas\"\n"
-  "default_animation: \"player_idle_down\"\n"
+  data: "tile_set: \"/main/pokemon.atlas\"\n"
+  "default_animation: \"pokemon_walk_down\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -37,13 +22,13 @@ embedded_components {
   id: "collision"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "type: COLLISION_OBJECT_TYPE_TRIGGER\n"
   "mass: 0.0\n"
   "friction: 0.0\n"
   "restitution: 0.0\n"
-  "group: \"character\"\n"
+  "group: \"pokemon\"\n"
   "mask: \"not_passable\"\n"
-  "mask: \"pokemon\"\n"
+  "mask: \"player\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
@@ -61,8 +46,8 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 16.0\n"
-  "  data: 23.0\n"
+  "  data: 8.0\n"
+  "  data: 8.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
